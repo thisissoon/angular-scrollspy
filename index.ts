f068022c7f7b4c0e6cfd5b6813fa869ae8d32912
Ed Opare-Aryee <ed@thisissoon.com>
2018-02-19 17:55:58 +0000

    build: update to ng-packagr@2.0.0

--- index.ts
+++
@@ -1,6 +0,0 @@
-export { ScrollSpyDirective } from './src/app/scroll-spy/scroll-spy/scroll-spy.directive';
-export { ScrollSpyItemDirective } from './src/app/scroll-spy/scroll-spy-item/scroll-spy-item.directive';
-export { ScrollSpySectionComponent } from './src/app/scroll-spy/scroll-spy-section/scroll-spy-section.component';
-export { ScrollSpyService } from './src/app/scroll-spy/scroll-spy-service/scroll-spy.service';
-export { Spy } from './src/app/scroll-spy/shared/spy.model';
-export { ScrollSpyModule } from './src/app/scroll-spy/scroll-spy.module';
